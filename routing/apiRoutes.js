f53283b772d5a687337da7db16007e24a6e9e845
Garnet <garnet.dianthus@gmail.com>
2018-09-12 13:59:28 -0700

    updates

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,83 +1,50 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
+// a POST routes  /api/friends 
+//load data
 
+var friendsData = require("../data/friend.js");
 
-
-var friendsData = require("app/data/friends");
-
-
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+  // GET route displays friends JSON
 
-
-  //THIS SHOULD DISPLAY A JSON OF ALL POSSIBLE FRIENDS........
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
   });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
 
-  
+    var friendScores = req.body.scores;  //body needs a reference
+    var scoreArray = [];
+    var friendSurveySum = 0;
+    var bestMatch = 0;
 
-    // if (tableData.length < 5) {
-    //   tableData.push(req.body);
-    //   res.json(true);
-    // }
-    // else {
-    //   waitListData.push(req.body);
-    //   res.json(false);
-    // }
+    // loop through friends in list
+    for (var i=0; i<friendsData.length; i++) {
+        var scoresDifference = 0;
+        //check and compare scores
+        for(var j = 0; j<friendScores.length; j++)
+            scoresDifference += (Math.abs(parseInt(friendData[i].scores[j])- parseInt(friendScores[j])));
+    }
+        // reults into scoreArray
   });
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+  scoreArray.push(scoresDifference);
+}
 
+//find match
+for(var i=0; i<scoreArray.length; i++){
+    if(scoreArray[i] <= scoreArray[bestMatch]){
+        bestMatch = i;
+    }
+}
 
-    // Empty out the arrays of data
-    // tableData.length = [];
-    // waitListData.length = [];
+// return bestMatch 
+var besties = friendsData[bestMatch];
+res.json(bestie);
 
+// new entry into friendsData array
+friendsData.push(req.body);
 
-
-    handle incoming survey results
-    handle compatability logic
-
-    res.json({ ok: true });
-  });
-};
-
-
-
-
-
-Your apiRoutes.js file should contain two routes:
-
-
-
-A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
+   
 
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,27 +1,19 @@
 
-
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
+//Dependencies
 var path = require("path");
 
-
-// ===============================================================================
 // ROUTING
-// ===============================================================================
+
 
 module.exports = function(app) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
 
+// A GET Route to /survey which should display the survey page.
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
  
-  // If no matching route is found default to home
+// A default, catch-all route that leads to home.html which displays the home page. 
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
@@ -30,8 +22,6 @@ module.exports = function(app) {
 
 
 
-// A GET Route to /survey which should display the survey page.
-// A default, catch-all route that leads to home.html which displays the home page. 
 
 
 
