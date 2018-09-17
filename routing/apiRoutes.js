c87da468b03452c6b5e126c8ce458f66ae9034b8
Garnet <garnet.dianthus@gmail.com>
2018-09-16 18:54:36 -0700

    update

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -12,8 +12,8 @@ module.exports = function (app) {
         console.log(friendsData);
     });
 
-//API POST REQUESTS
 
+//req.body is a part of the body-parser middleware. //req is the rest request. res is the response
     app.post("/api/friends", function (req, res) {
 
         var friendScores = req.body.scores;  //body needs a reference
@@ -22,16 +22,24 @@ module.exports = function (app) {
         var bestMatch = 0;
 
         // loop through friends in list
+        // for the friendsData check the scores
+ 
         for (var i = 0; i < friendsData.length; i++) {
+            if(friendsData[i].name === req.body.name){
+                continue;
+            }
             var scoresDifference = 0;
+            
             //check and compare scores
-            for (var j = 0; j < friendScores.length; j++)
+            //take absolute value of the difference between scores
+            for (var j = 0; j < friendScores.length; j++){
                 scoresDifference += (Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(friendScores[j])));
+            }
+            scoreArray.push(scoresDifference); //putting the result in the array after each calculation, for each friend in list
         }
 
-        scoreArray.push(scoresDifference);
-        // reults into scoreArray
-        //find match
+        //loop through the scoreArray to find the score that has the lowest score difference
+        //this logic isn't working
         for (var i = 0; i < scoreArray.length; i++) {
             if (scoreArray[i] <= scoreArray[bestMatch]) {
                 bestMatch = i;
@@ -39,16 +47,34 @@ module.exports = function (app) {
         }
 
         // return bestMatch 
-        var besties = friendsData[bestMatch];
-        res.json(besties);
-        console.log(besties);
+        var bestie = friendsData[bestMatch];
+        console.log(bestie);
+        friendsData.push(req.body);
+        res.json(bestie);     //returns a single bestie
+        
 
         // new entry into friendsData array
-        friendsData.push(req.body);
+      
 
     });
 }
 
 
 
+//GET POST PUT DELETE = main verbs 
+// var bestDifference = 999;
+
+// for (var i = 0; i <friendsData.length; i ++){
+//     var scoresDifference = 0;
+
+
+//     for (var j = 0; j < friendScores.length; j++){
+//         scoresDifference += (Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(friendScores[j])));
+//     }
+//     if (scoresDifference <= bestDifference){
+//         bestDifference = scoresDifference;        //this finds the better match each time it goes through the loop.
+//         bestMatch = i;                            //current bestMatch
+
+//     }         
+// });
 
